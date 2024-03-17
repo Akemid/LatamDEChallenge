c2318a7cac2fa98310ed41056b96e5725b8121df
Sergio Mondragon <smondragons@valtx.pe>
2024-03-17 00:46:27 -0500

    Refactor q3_memory function to improve performance and readability

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -6,6 +6,7 @@ from functools import reduce
 
 @profile(precision=4)
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
+    # Read the file in chunks and then process the chunks
     tweets = pd.read_json(
         file_path,
         lines=True,
@@ -35,17 +36,23 @@ def q3_memory(file_path: str) -> List[Tuple[str, int]]:
             "mentionedUsers": "object",
         },
     )
-
+    # Each chunk is processed and then added to the result
     user_list = map(process_tweets, tweets)
+    # The result is reduced to a single list
     result = reduce(lambda x, y: x + y, user_list)
+    # Count the users using Counter
     counter_mentioned_users = Counter(result)
+    # Get the top 10 users
     top_influyent_users = counter_mentioned_users.most_common(10)
     return top_influyent_users
 
 
 def process_tweets(df: pd.DataFrame):
+    # Select the column mentionedUsers
     df = df.loc[:, ["mentionedUsers"]].reset_index()
+    # Get the usernames from the column mentionedUsers
     df = df["mentionedUsers"].apply(lambda x: get_usernames(x))
+    # Flatten the list of users
     user_extracted = [
         user
         for user_list in df.values.tolist()
@@ -56,7 +63,9 @@ def process_tweets(df: pd.DataFrame):
 
 
 def get_usernames(df: pd.DataFrame):
+    # Get the username from the column mentionedUsers
     if df:
+        # Get the username and avoid keys that we are not going to use
         list_of_users = [user["username"] for user in df]
         return list_of_users
     return []
